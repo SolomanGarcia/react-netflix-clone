64cc9bf3ae04c595153a6bc3d887911ffdec8de4
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-21 10:35:03 -0600

    work on Row.js

---
+++ src/Row.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function Row({ title }) {
+  const [movies, setMovies] = useState([]);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      {/* container -> posters  */}
+    </div>
+  );
+}
+
+export default Row;

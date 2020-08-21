9e7b160cd7352920fdbb08458d3859a65376814e
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-21 11:08:57 -0600

    more work on Row.js

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
@@ -9,16 +11,27 @@ function Row({ title, fetchUrl }) {
     // if [], run once when the row loads, and don't run again
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+  console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
-      {/* container -> posters  */}
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }

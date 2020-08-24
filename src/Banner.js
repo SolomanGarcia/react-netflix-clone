6c27d91a2d1515f74f3753843c704d72f23a3027
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-24 10:56:20 -0600

    style banner/add truncate

--- src/Banner.js
+++ src/Banner.js
@@ -18,7 +18,9 @@ function Banner() {
     fetchData();
   }, []);
 
-  console.log(movie);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
     <header
@@ -40,7 +42,7 @@ function Banner() {
           <button className="banner__button">My List</button>
         </div>
         {/* description */}
-        <h1 className="banner__description">{movie.overview}</h1>
+        <h1 className="banner__description">{truncate(movie.overview, 150)}</h1>
       </div>
     </header>
   );

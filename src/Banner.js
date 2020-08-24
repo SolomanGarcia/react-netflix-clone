ba7694b2585d8702fd023b51b2a07ff67dfca1a4
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-24 10:18:30 -0600

    work on banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-import request from "./requests";
 import requests from "./requests";
 
 function Banner() {
@@ -9,9 +8,16 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
     }
     fetchData();
   }, []);
+
+  console.log(movie);
 
   return (
     <header>

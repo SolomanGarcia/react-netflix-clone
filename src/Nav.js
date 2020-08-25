03527d84852dbf17ca953714a1d0b2336528846c
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-25 14:29:23 -0600

    create Nav component

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Logo_Netflix.png/800px-Logo_Netflix.png"
+        alt="Netflix Logo"
+        className="nav__logo"
+      />
+
+      <img
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="Avatar"
+        className="nav__avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;

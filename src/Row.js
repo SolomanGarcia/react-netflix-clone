b6a5eca93c0b84d512a094b5a98716184add9f27
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-26 19:43:16 -0600

    work on popup

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // a snippet of code which runs on a specific condition/variable
   useEffect(() => {
@@ -17,6 +19,15 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1
+    }
+  };
 
   return (
     <div className="row">
@@ -34,6 +45,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }

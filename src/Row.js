e970407b08ebb08217b4bde24b468ab4e8eb0605
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-28 09:30:24 -0600

    debug

--- src/Row.js
+++ src/Row.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -36,7 +37,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
       movieTrailer(movie?.name || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl{Params.get("v")};
+          setTrailerUrl(urlParams.get("v"));
         })
         .catch((error) => console.log(error));
     }
